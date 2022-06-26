21996d947dc08658c1b7fd08a58c6cf46a8444bd
Fadhil Dzaky <86292434+FFreezed@users.noreply.github.com>
2022-06-27 00:04:21 +0700

    handle empty input ngab

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,7 +5,8 @@ function Form({ amount, setAmount }) {
 
   const AddAmount = e => {
     e.preventDefault();
-    
+    if(price.current.value === '') return;
+
     setAmount([...amount, {
       "price": price.current.value
     }]);
@@ -16,7 +17,7 @@ function Form({ amount, setAmount }) {
   return (
     <form className='form-control' onSubmit={AddAmount}> 
         <input className='form-input' type="number" name='amount' id='amount' placeholder='Masukkan angka...' ref={price}/>
-        <input className='form-submit' type="submit" value="Tambahkan" />
+        <input className='form-submit' type="submit" value="Tambahkan"/>
     </form>
   )
 }

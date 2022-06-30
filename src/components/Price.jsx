2c0c993564c1e09ecc358267ae8b084faa4d1016
Fadhil Dzaky <86292434+FFreezed@users.noreply.github.com>
2022-06-30 23:43:04 +0700

    nothing

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -6,14 +6,13 @@ function Price({ amount, index, removeAmount}) {
   }
   
   const sign = amount.price < 0 ? '-' : '+';
-
-  //if price is negative, change text from in to out
+  
   const text = amount.price < 0 ? 'Out' : 'In';
 
   return (
     <>
     <div className={amount.price < 0 ? 'minus' : 'plus'}>
-      <a className="remove-price" onClick={() => removeHandle(index)}>x </a>
+      <a className="remove-price" onClick={() => removeHandle(index)}>x</a>
       <span className={amount.price < 0 ? 'out' : 'in'}>{text}</span>
       <div className="list">{sign}Rp{Math.abs(amount.price)}</div>
     </div>
